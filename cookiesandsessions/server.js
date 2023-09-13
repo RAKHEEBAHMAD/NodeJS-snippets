7154ba4d521ebd0e43d2a6d7fab30ae6bfec796b
rakheeb <rakheeb1905@gmail.com>
2023-09-14 00:31:43 +0530

    cookies and sessions

--- cookiesandsessions/server.js
+++ cookiesandsessions/server.js
@@ -1,30 +1,42 @@
-const express = require('express')
-const cookieparser = require('cookie-parser')
+const express = require("express");
+const cookieparser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cookieparser());
 
-app.get('/',(req,res)=>{
-    res.send("hello asif bhai")
-})
-
-app.get('/setcookie', (req, res) => {
-    res.cookie(`Cookie token name`,`encrypted cookie string Value`,{
-        maxAge: 5000,
-        // expires works the same as the maxAge
-        expires: new Date('01 12 2021'),
-        secure: true,
-        httpOnly: true,
-        sameSite: 'lax'
-    });
-    res.send('Cookie have been saved successfully');
+app.get("/", (req, res) => {
+  res.send("hello asif bhai");
 });
 
-app.get('/displaycookies',(req,res)=>{
-    res.send(req.cookies);
-})
+app.get("/setcookie", (req, res) => {
+  const sessionid = uuidv4();
+  res.cookie(`sid`, sessionid, {
+    maxAge: 60*60*24*7,
+    // expires works the same as the maxAge
+    //expires: new Date("12 3 2024"),
+    secure: true,
+    httpOnly: true,
+    sameSite: "lax",
+  });
+  res.send("Cookie have been saved successfully");
+});
 
-app.listen(4000,()=>{
-    console.log("Server listening");
-})
+app.get("/displaycookies", (req, res) => {
+  res.send(req.cookies);
+});
+
+app.get("/clearcookies", (req, res) => {
+  const cookies = req.cookies;
+
+  for (const cookieName in cookies) {
+    res.clearCookie(cookieName);
+  }
+
+  res.send("All cookies cleared.");
+});
+
+app.listen(4000, () => {
+  console.log("Server listening");
+});
