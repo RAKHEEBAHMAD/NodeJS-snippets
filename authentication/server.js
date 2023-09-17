c17b0e3fa9a6d924dcfbfa373f2d4bf1c12cdc20
rakheeb <rakheeb1905@gmail.com>
2023-09-17 19:54:48 +0530

    authentication

--- authentication/server.js
+++ authentication/server.js
@@ -1,102 +1,113 @@
-const express = require('express')
-const authmodel = require('./models/auth')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const cookieparser = require('cookie-parser')
+const express = require("express");
+const authmodel = require("./models/auth");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
+const bodyparser = require('body-parser')
+const {validtoken,isauthenticated} = require('./services/service')
+
+const app = express();
+mongoose
+  .connect("mongodb://127.0.0.1:27017/authentication")
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
-const app = express()
-mongoose.connect('mongodb://127.0.0.1:27017/authentication')
-    .then(()=>{
-        console.log('db connected')
-    }).catch((err)=>{
-        console.log(err)
-    })
-    
-
-app.set('view engine','ejs')
-app.set('views','./authentication/views')
-app.use(express.urlencoded({extended:false}));
+app.set("view engine", "ejs");
+app.set("views", "./authentication/views");
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
 
 
+app.get("/",validtoken(), (req, res) => {
+  res.render("home");
+});
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+app.get("/login",isauthenticated(), (req, res) => {
+  res.render("login", { error: null });
+});
 
-app.get('/login',(req,res)=>{
-    res.render('login',{error:null})
-})
+app.get("/signup",isauthenticated(), (req, res) => {
+  res.render("signup.ejs", { error: null });
+});
 
-app.get('/signup',(req,res)=>{
-    res.render('signup.ejs',{error:null})
-})
-
-app.post('/signup',async(req,res)=>{
-    const username =req.body.username
-    const email =req.body.email
-    const password = req.body.password
-    const hashedpassword =await bcrypt.hash(password,10)
-    try{
-        const user = await authmodel.findOne({email})
-        if(user)
-        {
-            return res.status(500).render('signup',{error:"User already exists"})
-        }
-        const newuser = await authmodel.create(
-            {
-                username,email,password:hashedpassword
-            }
-        )
-        const token = await jwt.sign({
-            username: newuser.username,
-            email: newuser.email
-        },'supersecret')
-        res.cookie('token',token)
-        console.log(req.cookies)
-        res.redirect('/')
-    }catch(err)
-    {
-        console.log(err)
-        res.send(err)
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedpassword = await bcrypt.hash(password, 10);
+  try {
+    const user = await authmodel.findOne({ email });
+    if (user) {
+      return res.status(500).render("signup", { error: "User already exists" });
     }
-})
+    const newuser = await authmodel.create({
+      username,
+      email,
+      password: hashedpassword,
+    });
+    const token = await jwt.sign(
+      {
+        username: newuser.username,
+        email: newuser.email,
+      },
+      "supersecret"
+    );
+    res.cookie("sid", token,{secure:false});
+    console.log(req.cookies);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
 
-app.post('/login',async(req,res)=>{
-    const {email,password} = req.body
-    try{
-        const user = await authmodel.findOne({email})
-        if(!user)
-        {
-            return res.status(500).render('login',{error:"incorrect credentials"})
-        }
-        const checkpassword= await bcrypt.compare(password,user.password)
-        if(!checkpassword)
-        {
-            return res.render('login',{error:"incorrect credentials"})
-        }
-        const token = await jwt.sign({
-            username: user.username,
-            email: user.email
-        },'supersecret')
-        user.token = token
-        console.log(user.token)
-        res.cookie('token',token)
-        console.log(req.cookies)
-        return res.redirect('/')
-    } catch (err) {
-        console.error('Error:', err);
-        res.status(500).send(err);
+app.get("/show-cookies", (req, res) => {
+    const cookies = req.cookies;
+    res.json(cookies);
+  });
+  
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await authmodel.findOne({ email });
+    if (!user) {
+      return res
+        .status(500)
+        .render("login", { error: "incorrect credentials" });
     }
-})
+    const checkpassword = await bcrypt.compare(password, user.password);
+    if (!checkpassword) {
+      return res.render("login", { error: "incorrect credentials" });
+    }
+    const token = await jwt.sign(
+      {
+        username: user.username,
+        email: user.email,
+      },
+      "supersecret"
+    );
+    user.token = token;
+    res.cookie("sid", token);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send(err);
+  }
+});
 
-app.get('/logout',(req,res)=>{
-    const token = 'token'
-    res.clearCookie(token)
-    res.redirect('/login')
-})
+app.get("/logout", (req, res) => {
+  const token = "sid";
+  res.clearCookie(token);
+  res.redirect("/login");
+});
 
-
-app.listen(3000,()=>{ console.log('server listening')})
+app.listen(3000, () => {
+  console.log("server listening");
+});
--- authentication/services/service.js
+++ authentication/services/service.js
@@ -1,1 +1,49 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const expsession = require('express-session')
+
+
+function validtoken() {
+  return async (req, res, next) => {
+    const token = req.cookies?.sid;
+
+    if (!token) {
+      return res.redirect("/login");
+    }
+
+    try {
+      const decoded = jwt.verify(token, "supersecret");
+      if (decoded === null) {
+        return res.redirect("/login");
+      }
+      req.user = decoded;
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.redirect("/login");
+    }
+  };
+}
+
+function isauthenticated() {
+  return async (req, res, next) => {
+    const token = req.cookies?.sid;
+    if (!token) {
+      next()
+    }
+
+    try {
+        const decoded = jwt.verify(token, "supersecret");
+        if (decoded === null) {
+          next()
+        }
+        req.user = decoded;
+        return res.redirect('/')
+      } catch (error) {
+        console.log(error);
+        next()
+      }
+
+  };
+}
+
+module.exports = { validtoken,isauthenticated};
